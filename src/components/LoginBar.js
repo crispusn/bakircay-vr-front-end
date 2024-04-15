c1ddab4e12ef99b739b8f300edb9a7082840b2eb
Kaan Soylev <kaansoylev@gmail.com>
2024-04-15 12:28:08 +0300

    changes

--- src/components/LoginBar.js
+++ src/components/LoginBar.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Text, Html } from "@react-three/drei";
+import { Interactive } from "@react-three/xr";
 
 function LoginBar() {
   const [username, setUsername] = useState(""); // Kullanıcı adı state'i
   const [password, setPassword] = useState(""); // Şifre state'i
+  const usernameInputRef = useRef(null); // Kullanıcı adı input alanı referansı
+  const passwordInputRef = useRef(null); // Şifre input alanı referansı
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -13,6 +16,13 @@ function LoginBar() {
     setPassword(event.target.value);
   };
 
+  const handleInputClick = (ref) => {
+    // Tıklama durumunda ilgili input alanının referansına odaklan
+    ref.current.focus();
+    // Tıklama durumunda bir console log yap
+    console.log("Input bara tıklandı!");
+  };
+
   const handleSubmit = () => {
     // Form gönderimine ilişkin işlemleri burada gerçekleştirebilirsiniz
     console.log("Kullanıcı adı:", username);
@@ -20,71 +30,83 @@ function LoginBar() {
   };
 
   return (
-    <group position={[0, 1, -4]}>
-      {/* Siyah arka plan */}
-      <mesh position={[0, 0, 0]} scale={[14, 8, 0.1]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="black" />
-      </mesh>
-      {/* Kullanıcı adı giriş alanı */}
-      <mesh position={[0, 1, 0.2]} scale={[5, 0.5, 0.2]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="white" />
-        <Html>
-          {" "}
-          <input
-            style={{
-              width: "100%",
-              height: "100%",
-              border: "none",
-              background: "transparent",
-            }}
-            type="text"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </Html>
-      </mesh>
-
-      {/* Şifre giriş alanı */}
-      <mesh position={[0, 0, 0.2]} scale={[5, 0.5, 0.2]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="white" />
-        <Html>
-          {" "}
-          <input
-            style={{
-              width: "100%",
-              height: "100%",
-              border: "none",
-              background: "transparent",
-            }}
-            type="text"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </Html>
-      </mesh>
-
-      {/* Giriş butonu */}
-      <mesh
-        position={[0, -1, 0.2]}
-        scale={[5, 0.5, 0.2]}
-        onClick={handleSubmit}
-      >
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="blue" />
-        <Text
-          position={[0, 0, 0.1]}
-          fontSize={0.2}
-          color="white"
-          anchorX="center"
-          anchorY="middle"
+    <Interactive onSelect={() => console.log("LoginBar'a tıklandı!")}>
+      <group position={[0, 1, -4]}>
+        {/* Siyah arka plan */}
+        <mesh position={[0, 0, 0]} scale={[14, 8, 0.1]}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        {/* Kullanıcı adı giriş alanı */}
+        <mesh
+          position={[0, 1, 0.2]}
+          scale={[5, 0.5, 0.2]}
+          onClick={() => handleInputClick(usernameInputRef)}
         >
-          Giriş
-        </Text>
-      </mesh>
-    </group>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="white" />
+          <Html>
+            {" "}
+            <input
+              ref={usernameInputRef}
+              style={{
+                width: "100%",
+                height: "100%",
+                border: "none",
+                background: "transparent",
+              }}
+              type="text"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </Html>
+        </mesh>
+
+        {/* Şifre giriş alanı */}
+        <mesh
+          position={[0, 0, 0.2]}
+          scale={[5, 0.5, 0.2]}
+          onClick={() => handleInputClick(passwordInputRef)}
+        >
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="white" />
+          <Html>
+            {" "}
+            <input
+              ref={passwordInputRef}
+              style={{
+                width: "100%",
+                height: "100%",
+                border: "none",
+                background: "transparent",
+              }}
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </Html>
+        </mesh>
+
+        {/* Giriş butonu */}
+        <mesh
+          position={[0, -1, 0.2]}
+          scale={[5, 0.5, 0.2]}
+          onClick={handleSubmit}
+        >
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="red" />
+          <Text
+            position={[0, 0, 0.1]}
+            fontSize={0.6}
+            color="white"
+            anchorX="center"
+            anchorY="middle"
+          >
+            Giriş
+          </Text>
+        </mesh>
+      </group>
+    </Interactive>
   );
 }
 
