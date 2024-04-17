29d7573b24104329ae12ea511902536ae170cc15
Kaan Soylev <kaansoylev@gmail.com>
2024-04-17 11:27:05 +0300

    changes

--- src/components/LoginBar.js
+++ src/components/LoginBar.js
@@ -1,14 +1,11 @@
 import { Form, Input, Label, Submit } from "r3f-form";
-import { Html } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { Interactive } from "@react-three/xr";
 
 function LoginBar() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const usernameInputRef = useRef(null);
-  const passwordInputRef = useRef(null);
   const { size } = useThree();
   const aspect = size.width / size.height;
   const boxWidth = 14;
@@ -16,60 +13,65 @@ function LoginBar() {
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
+    console.log("Username:", event.target.value);
   };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
+    console.log("Password:", event.target.value);
   };
 
-  const handleInputClick = (ref) => {
-    ref.current.focus();
-    console.log("Input bara tıklandı!");
+  const handleSubmit = () => {
+    console.log("Kullanıcı adı:", username);
+    console.log("Şifre:", password);
+    console.log("Login butonuna tıklandı!");
   };
 
-  const handleSubmit = (formData) => {
-    console.log("Kullanıcı adı:", formData.username);
-    console.log("Şifre:", formData.password);
-    console.log("Login butonuna tıklandı!"); // Yeni eklendi
-    // Giriş işlemlerini gerçekleştir
-  };
+  const formWidth = 6;
+  const formHeight = 2.5;
 
-  const formWidth = 6; // Form genişliği
-  const formHeight = 2.5; // Form yüksekliği
-
-  const formPosX = -formWidth / 2; // Formun sol üst köşesinin x konumu
-  const formPosY = formHeight / 2 - 0.5; // Formun sol üst köşesinin y konumu
-
-  const inputSpacing = 0.7; // Form elemanları arası boşluk
+  const formPosX = -formWidth / 2;
+  const formPosY = formHeight / 2 - 0.5;
 
   return (
-    <Interactive>
-      <group position={[0, 1, -4]}>
-        <mesh position={[0, 0, -1]} scale={[boxWidth, boxHeight, 0.1]}>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="black" />
-        </mesh>
-        <Form position={[formPosX, formPosY, 0]} onSubmit={handleSubmit}>
-          <Label text="username" position={[0, 0.8, 0]} color="white" />
+    <group position={[0, 1, -2]}>
+      <mesh position={[0, 0, -1]} scale={[boxWidth, boxHeight, 0.1]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="black" />
+      </mesh>
+      <Form position={[formPosX, formPosY, 0]} onSubmit={handleSubmit}>
+        <Label text="username" position={[0, 0.8, 0]} color="white" />
+        <Interactive
+          onSelectStart={() => console.log("Username alanına tıklandı")}
+        >
           <Input
+            selectionColor={"red"}
             name="username"
             position={[0, 0.4, 0]}
             width={5}
             height={0.5}
+            onChange={handleUsernameChange}
           />
+        </Interactive>
 
-          <Label text="password" position={[0, -0.2, 0]} color="white" />
+        <Label text="password" position={[0, -0.2, 0]} color="white" />
+        <Interactive
+          onSelectStart={() => console.log("Password alanına tıklandı")}
+        >
           <Input
             name="password"
             type="password"
             position={[0, -0.6, 0]}
             width={5}
             height={0.5}
+            onChange={handlePasswordChange}
           />
+        </Interactive>
+        <Interactive onSelectStart={handleSubmit}>
           <Submit value="Login" position={[0, -1, 0]} width={4} height={0.5} />
-        </Form>
-      </group>
-    </Interactive>
+        </Interactive>
+      </Form>
+    </group>
   );
 }
 
